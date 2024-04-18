1f14775b608ae2b43f7a3f4ce92f7bf1ecbaf473
seabee33 <85791528+seabee33@users.noreply.github.com>
2024-04-18 14:11:34 +1000

    Update exampleERC20Token.sol

--- testing/exampleERC20Token.sol
+++ testing/exampleERC20Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("Testicle", "TEST") {
+    constructor() ERC20("Full Name", "Ticker") {
         _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
 }
